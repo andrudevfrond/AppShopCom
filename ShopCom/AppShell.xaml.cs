e55e5026152b844340fbc3cb16443e648dcf1e10
Andres Galindo <sharlly1329g@gmail.com>
2024-07-25 11:10:36 -0500

    Funcionalidades adicionales en la pagina de Shell

--- ShopCom/AppShell.xaml.cs
+++ ShopCom/AppShell.xaml.cs
@@ -6,5 +6,11 @@
         {
             InitializeComponent();
         }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var uri = new Uri("https://vaxdrez.com");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
     }
 }
