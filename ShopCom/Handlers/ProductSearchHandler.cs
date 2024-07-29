276d59a2a70e218613df4db3108c9c42e682eef2
Andres Galindo <sharlly1329g@gmail.com>
2024-07-28 21:21:04 -0500

    Implemetar busqueda de productos

---
+++ ShopCom/Handlers/ProductSearchHandler.cs
@@ -0,0 +1,37 @@
+﻿using ShopCom.DataAccess;
+using ShopCom.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCom.Handlers
+{
+    public class ProductSearchHandler : SearchHandler
+    {
+        ShopDbContext db;
+
+        public ProductSearchHandler()
+        {
+            this.db = new ShopDbContext();
+        }
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            if (string.IsNullOrWhiteSpace(newValue)) { 
+                ItemsSource = null;
+                return;
+            }
+
+            var results = db.Products.Where(p => p.Name.ToLowerInvariant().Contains(newValue.ToLowerInvariant())).ToList();
+
+            ItemsSource = results;
+
+        }
+
+        protected override async void OnItemSelected(object item)
+        {
+           await Shell.Current.GoToAsync($"{nameof(ProductDetailPage)}?id={((Product)item).Id}");
+        }
+    }
+}
